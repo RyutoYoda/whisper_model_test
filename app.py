e536fa24c6c7afb4517392595b86e219dfa6dbee
RyutoYoda <98861847+RyutoYoda@users.noreply.github.com>
2024-05-16 12:36:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def load_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
 
-image_path = "/mnt/data/A_cute_cat_wearing_headphones,_depicted_in_a_wide_.png"  # 画像ファイルのパスを指定
+image_path = "スクリーンショット 2024-05-16 12.33.11.png"  # 画像ファイルのパスを指定
 image_base64 = load_image(image_path)
 st.markdown(
     f"""
